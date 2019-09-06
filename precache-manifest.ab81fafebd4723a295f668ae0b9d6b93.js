0aacb9669cc6be4555c480e9954a4e8c2737f355
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2019-09-06 16:57:26 +0530

    Updates

---
+++ precache-manifest.ab81fafebd4723a295f668ae0b9d6b93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffdf45d8d2eea96699c454871fee88f0",
+    "url": "/react-native-drag-and-drop-ui-creator/index.html"
+  },
+  {
+    "revision": "31fe712a5c9c209fcd0c",
+    "url": "/react-native-drag-and-drop-ui-creator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "96ee86fbb094cb1bbbdc",
+    "url": "/react-native-drag-and-drop-ui-creator/static/js/2.7b256ad1.chunk.js"
+  },
+  {
+    "revision": "31fe712a5c9c209fcd0c",
+    "url": "/react-native-drag-and-drop-ui-creator/static/js/main.78f898ca.chunk.js"
+  },
+  {
+    "revision": "f944d547a7472e3320a9",
+    "url": "/react-native-drag-and-drop-ui-creator/static/js/runtime~main.663591f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-native-drag-and-drop-ui-creator/static/media/logo.5d5d9eef.svg"
+  }
+]);
